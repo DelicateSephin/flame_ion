3c6a4fa40ee8096c5a10260d4e0204a7f5138b45
DelicateSephin <48096249+DelicateSephin@users.noreply.github.com>
2019-12-17 21:20:39 +0900

    0.0.2 update

--- lib/ion_game.dart
+++ lib/ion_game.dart
@@ -6,7 +6,7 @@ part of flame_ion;
 
 abstract class IonGame extends flame.Game{
   /// 현재 화면 크기입니다.
-  static ui.Size screenSize = ui.Size.zero;  
+  static ui.Size screenSize = ui.Size.zero;
 
   /// 현재 화면이 가로 화면인지 확인합니다.
   /// (true == 가로 화면)
@@ -23,11 +23,36 @@ abstract class IonGame extends flame.Game{
   }
 
 
-  final IonHelper helper = IonHelper();
+  /// [IonHelper]는 보다 편한 게임 제작을 위해 제공되는
+  /// 헬퍼 클래스입니다. [IonHelper]를 이용하면 게임의
+  /// 화면 조작을 비롯한 많은 작업들이 자동으로 수행됩니다.
+  /// [IonHelper]를 이용할 경우 사용자는 [IonGame]의
+  /// 서브클래스에서 [IonHelper]를 이용하여 게임을 관리합니다.
+  /// [IonHelper]의 자동 처리 기능을 이용하고 싶지 않다면,
+  /// 사용자는 [IonGame]의 서브클래스에서 [update], [render]
+  /// 함수를 재정의하고, 직접 게임에서 사용될 [IonView]들을
+  /// 관리하여야 합니다. 이 경우에도 [IonHelper]의 각종 편의
+  /// 기능들을 제한적으로 이용할 수 있습니다.
+  IonHelper helper = IonHelper();
+
+  @override
+  void update(double t)
+  {
+    assert(t != null);
+    helper.update(t);
+  }
+
+  @override
+  void render(ui.Canvas canvas)
+  {
+    assert(canvas != null);
+    helper.render(canvas);
+  }
 
   @override
   void resize(ui.Size size)
   {
+    assert(size != null);
     screenSize = size;
   }
 }
--- lib/ion_helper.dart
+++ lib/ion_helper.dart
@@ -4,7 +4,48 @@
 part of flame_ion;
 
 
+/// [IonHelper]는 보다 편한 게임 제작을 위해 제공되는
+/// 헬퍼 클래스입니다. [IonHelper]를 직접 생성하는 것
+/// 보다는 [IonGame]에 생성되어 있는 [helper] 객체를
+/// 이용하는 것을 추천합니다.
 class IonHelper{
+  /// [currentView]의 값은 다음을 의미합니다.
+  /// [currentView]의 값을 변경할 때, 직접 값을
+  /// 입력하는 대신 다음 상수들을 이용합니다.
+  /// ex) 만약 currentView가 0이면, 현재 화면에
+  /// 렌더링 된 [IonView]는 홈 화면 입니다.
+  static const home_view = 0;
+  static const game_view = 1;
+  static const setting_view = 2;
+  static const credit_view = 3;
+
+
+  /// [IonHelper]를 이용한 Flame Ion 게임에서,
+  /// 게임의 화면 처리는 자동으로 처리되며 이를
+  /// 위해선 사용자는 [IonHelper]의 [views]에
+  /// [IonView]들을 추가하여야 합니다.
+  List<IonView> views = List<IonView>();
+  /// [IonHelper]를 이용한 Flame Ion 게임에서
+  /// 현재 화면에 렌더링 된 [IonView]를 나타냅니다.
+  /// 기본 설정은 홈 화면입니다.
+  int currentView = home_view;
+
+  /// [views]에 포함된 [IonView]들을 업데이트합니다.
+  void update(double t)
+  {
+    assert(t != null);
+    for(var obj in views){
+      obj.update(t);
+    }
+  }
+
+  /// 현재 화면에 나올 [IonView]를 렌더링합니다.
+  void render(ui.Canvas canvas)
+  {
+    assert(canvas != null);
+    views[currentView].render(canvas);
+  }
+  
   /// 지원하는 스크린 모드를 설정합니다.
   /// [fullScreen] : 전체 화면
   /// [portraitUp] : 세운 세로 화면
@@ -28,5 +69,4 @@ class IonHelper{
     if(landscapeRight) orientations.add(flutter.DeviceOrientation.landscapeRight);
     await flame.Flame.util.setOrientations(orientations);
   }
-
 }
